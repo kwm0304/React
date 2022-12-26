7105e483c3ed74a56802312473f7487a7fc0d41e
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2022-12-26 13:54:51 -0500

    added resume component

--- 20dollarbill/src/components/Resume/index.js
+++ 20dollarbill/src/components/Resume/index.js
@@ -1,7 +1,33 @@
-import React from 'react'
-
-function Resume() {
+import React from 'react';
+import resume from '../../assets/resume.pdf'
+import { FaFileDownload } from 'react-icons/fa' 
+const Resume = () => {
+    // Function will execute on click of button
+    const onButtonClick = () => {
+        // using Java Script method to get PDF file
+        fetch('resume.pdf').then(response => {
+            response.blob().then(blob => {
+                // Creating new object of PDF file
+                const fileURL = window.URL.createObjectURL(blob);
+                // Setting various property values
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'resume.pdf';
+                alink.click();
+            })
+        })
+    }
+    return (
+        <>
+            <center>
+                <h1 className='resume'>Resume</h1>
+                <h3>Click on below button to download as PDF</h3>
+                <a href={resume} className="download-logo" download>
+                <FaFileDownload/>
+              </a>
+            </center>
+        </>
+    );
 
 }
-
-export default Resume
+export default Resume;
