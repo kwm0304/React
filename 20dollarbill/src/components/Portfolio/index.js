2c293e5e7d605fbc76d1bf1116ce545719df0c4a
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2022-12-26 12:03:24 -0500

    added portfolio projects

--- 20dollarbill/src/components/Portfolio/index.js
+++ 20dollarbill/src/components/Portfolio/index.js
@@ -1,7 +1,44 @@
 import React from 'react'
+import quiz from '../../assets/quiz.png'
+import scheduler from '../../assets/scheduler.png'
+import weather from '../../assets/weather.png'
+
 
 function Portfolio() {
+    return(
+        <div className="Projects">
 
+  <div className="card">
+    <div className="cardImgLink">
+    <h2 className='cardTitle'>Weather App</h2>
+      <a href="https://kwm0304.github.io/Weather-App/"><img className="cardImg" src={weather} style={{width: "33%"}}alt="weather forecast" /></a>
+    </div>
+      <div className="card-description">
+        This weather app was made using javascript and basic fetch methods to return a 5-day forecast for cities in the U.S.
+      </div>
+  </div>
+
+  <div className="card">
+    <div className="cardImgLink">
+    <h2 className='cardTitle'>Coding Quiz</h2>
+      <a href="https://kwm0304.github.io/Coding-quiz-app/"><img className="cardImg" src={quiz} style={{width: "20%"}} alt="computer quiz" /></a>
+    </div>
+      <div className="card-description">
+        This simple quiz game was made with vanilla javascript.
+      </div>
+  </div>
+
+  <div className="card">
+    <div className="cardImgLink">
+    <h2 className='cardTitle'>Work Day Scheduler</h2>
+      <a href="https://kwm0304.github.io/Day-Scheduler/"><img className="cardImg" src={scheduler} style={{width:"20%"}} alt="day planner" /></a>
+    </div>
+      <div className="card-description">
+        Made using moment.js and jquery, this provides app provides a real-time work day schedule to keep the user on track.
+      </div>
+  </div>
+</div>
+    )
 }
 
-export default Portfolio
+export default Portfolio;
