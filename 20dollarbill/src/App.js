f10c47780adac71c5ce85562f6f0ce7c70f1e96c
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2022-12-26 11:02:28 -0500

    Added props and page changes in header and nav, a little bit in about section

--- 20dollarbill/src/App.js
+++ 20dollarbill/src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [currentTab, setCurrentTab] = useState("about");
+
+	// This function checks to see which tab is selected and then generates the appropriate tab.
+	const renderTab = () => {
+		switch (currentTab) {
+			case "about":
+				return <About />;
+			case "portfolio":
+				return <Portfolio />;
+			case "contact":
+				return <Contact />;
+			case "resume":
+				return <Resume />;
+			default:
+				return <About />;
+		}
+	};
+
+	return (
+		<>
+			<div className="mobile-header">
+				<Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
+			</div>
+			<div>
+				<main>{renderTab()}</main>
+				<Footer></Footer>
+			</div>
+			</>
+	);
 }
 
 export default App;
---
+++ 20dollarbill/src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Nav from '../Nav';
+
+function Header() {
+const { currentTab, setCurrentTab } = props;
+
+return(
+    <div>
+        <section>
+            <header className='flex-row'>
+                <h1>Kenan McKenzie</h1>
+            <div>
+                <Nav currentTab={currentTab} setCurrentTab={setCurrentTab} />
+            </div>
+            </header>
+            <section className='backgroundImg'>
+                <div className='backgroundPic'></div>
+                <h2 className='welcome'></h2>
+                <p>Welcome to my React portfolio. 
+                    Take a look around!</p>
+            </section>
+        </section>
+    </div>
+)
+}
+
+export default Header
